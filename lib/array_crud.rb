9aa581dc4a00aa51567a4e3b091e2a932bb7fa39
Emily G <aspentree79@gmail.com>
2019-08-05 20:39:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
 my_array = ["Blanche", "Rose", "Sophia", "Dorothy"]
-my_array << "Stan"
+my_array << (element)
 end
 
 def add_element_to_start_of_array(array, element)
