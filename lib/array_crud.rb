fe12608f45e91313ec47f3ab82dab7ca26f8fb8f
Emily G <aspentree79@gmail.com>
2019-08-05 20:43:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,7 @@ my_array << (element)
 end
 
 def add_element_to_start_of_array(array, element)
-my_array.unshift(element)
+array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
